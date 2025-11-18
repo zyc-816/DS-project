76f62707b24233ae458df46e7275dde8a2b5921c
zyc-816 <860816655@qq.com>
2025-11-19 00:34:32 +0800

    2.0.1

--- src/2.cpp
+++ src/2.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-void initQue() {
-    char symbols[] = {'+', '-', '*', '/', '('};
+//获取一个随机题目
+//toDo：添加括号，算式合法性
+string getQues(int maxNum, int len) {
+    string symbols[] = {"+", "-", "*", "/"};
+    string ques = "";
+    int index = 0;
+    while(index < len) {
+        if(index % 2) {
+            int ranSym = rand()%5;
+            string symbol = symbols[ranSym];
+            ques.append(symbol);
+        }
+        else {
+            int ranNum = rand()%(maxNum+1);
+            string num = to_string(ranNum);
+            ques.append(num);
+        }
+        index++;
+    }
+    
+    return ques;
+}
 
+//初始化题库
+void initQues() {
+
+    return;
+}
+
+//计算问题
+int cal(string ques) {
+    int ans;
+    vector<int> OPND;
+    vector<char> OPTR;
+
+    return ans;
 }
 
 int main() {
-    initQue();
-
+    srand((unsigned)time(NULL));
+    initQues();
+    int i;
+    cin>>i;
+    while(i--) {
+        cout<<getQues(10, 5)<<endl;
+    }
 
     return 0;
 }
