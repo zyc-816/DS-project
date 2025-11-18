5e43c4ed48b78b6a8088da1fa28a61d23e930cfe
ioezzz <860816655@qq.com>
2025-11-18 09:43:49 +0000

    2.0.0

---
+++ src/2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+void initQue() {
+    char symbols[] = {'+', '-', '*', '/', '('};
+
+}
+
+int main() {
+    initQue();
+
+
+    return 0;
+}
