7aa85f8fe2d3730e0271fa75a19026124e64a173
zyc-816 <860816655@qq.com>
2025-11-19 21:52:53 +0800

    2.0.2 初步实现算式生成（存在多余括号问题）

--- src/2.cpp
+++ src/2.cpp
@@ -7,25 +7,65 @@
 using namespace std;
 
 //获取一个随机题目
-//toDo：添加括号，算式合法性
-string getQues(int maxNum, int len) {
+//toDo：去除多余括号，算式合法性
+string getSymbol() {
     string symbols[] = {"+", "-", "*", "/"};
+    int ranSym = rand()%4;
+    return symbols[ranSym];
+}
+
+string getNum(int maxNum) {
+    int ranNum = rand()%(maxNum+1);
+    return to_string(ranNum);
+}
+
+bool isSym(char chr) {
+    char sym[] = {'+', '-', '*', '/'};
+    for(int i=0; i<4; i++) {
+        if(chr == sym[i]) return true;
+    }
+    return false;
+}
+
+bool isNum(char chr) {
+    return chr >= '0' && chr <='9';
+}
+
+string getQues(int maxNum, int len, int parNum) {
+    
     string ques = "";
     int index = 0;
+    if(len % 2 == 0) len++;
     while(index < len) {
         if(index % 2) {
-            int ranSym = rand()%5;
-            string symbol = symbols[ranSym];
+            string symbol = getSymbol();
             ques.append(symbol);
         }
         else {
-            int ranNum = rand()%(maxNum+1);
-            string num = to_string(ranNum);
+            string num = getNum(maxNum);
             ques.append(num);
         }
         index++;
     }
-    
+
+    //插入括号
+    while(parNum--) {
+        int l = 0;
+        int r = 0;
+        while(l == r) {
+            l = rand()%len;
+            r = rand()%len;
+            if(l == r) continue;
+            l = min(l, r);
+            r = max(l, r);
+            //保证括号位置合理?
+            while(l != 0 && !isSym(ques[l-1])) l--;
+            while(r != len && isNum(ques[r])) r++;
+            ques.insert(l, "(");
+            ques.insert(r+1, ")");
+            len += 2;
+        }
+    }
     return ques;
 }
 
@@ -36,12 +76,11 @@ void initQues() {
 }
 
 //计算问题
-int cal(string ques) {
-    int ans;
+bool cal(string ques, int ans) {
     vector<int> OPND;
     vector<char> OPTR;
 
-    return ans;
+    return true;
 }
 
 int main() {
@@ -50,7 +89,7 @@ int main() {
     int i;
     cin>>i;
     while(i--) {
-        cout<<getQues(10, 5)<<endl;
+        cout<<getQues(10, 5, 1)<<endl;
     }
 
     return 0;
