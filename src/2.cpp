c254ae3a3d95d2e25cf083f1181d41ec8ea3d23b
zyc-816 <860816655@qq.com>
2025-11-20 12:37:16 +0800

    2.0.3

--- src/2.cpp
+++ src/2.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 #include <fstream>
 #include <string>
-#include <vector>
+#include <stack>
 using namespace std;
 
 //获取一个随机题目
@@ -76,9 +76,98 @@ void initQues() {
 }
 
 //计算问题
-bool cal(string ques, int ans) {
-    vector<int> OPND;
-    vector<char> OPTR;
+
+//运算符优先级比较
+bool compare(char chr, char top) {
+    if(top == '(') return true;
+    int pri_1 = 0;
+    int pri_2 = 0;
+    if(chr == '*' || chr == '/') pri_1 = 1;
+    if(top == '*' || top == '/') pri_2 = 1;
+    return pri_1 > pri_2;
+}
+
+//从操作数栈中获取前两个数
+void getTwo(int& a, int& b, stack<int>& OPND) {
+    a = OPND.top();
+    OPND.pop();
+    b = OPND.top();
+    OPND.pop();
+    return;
+}
+
+//处理过程中的基本算式
+bool baseCal(int a, int b, char optr, int& ans) {
+    if(optr == '+') ans = a + b;
+    else if(optr == '-') ans = a - b;
+    else if(optr == '*') ans = a * b;
+    else if(optr == '/') {
+        if(b == 0) {
+            return false;
+        }
+        else ans = a / b;
+    }
+
+    return true;
+}
+
+//计算问题
+bool calEq(string ques, int& ans) {
+    stack<int> OPND; //操作数栈
+    stack<char> OPTR; //操作符栈
+
+    //转化为后缀表达式
+    string rpn = "";
+    int len = ques.length();
+    for(int i=0; i<len; i++) {
+        char chr = ques[i];
+        if(isSym(chr) || chr == '(' || chr == ')') {
+            if(chr == ')') {
+                while(OPTR.top() != '(') {
+                    rpn.push_back(OPTR.top());
+                    OPTR.pop();
+                }
+                OPTR.pop();
+            }
+            else if(OPTR.empty() || compare(chr, OPTR.top())) {
+                OPTR.push(chr);
+            }
+            else {
+                while(!compare(chr, OPTR.top())) {
+                    rpn.push_back(OPTR.top());
+                    OPTR.pop();
+                }
+                OPTR.push(chr);
+            }
+        }
+        else if(isNum(chr)) {
+            rpn.push_back(chr);
+
+            //以空格隔开数字，便于处理
+            if(i+1 < len 
+                && (isSym(ques[i+1]) || chr == '(' || chr == ')')) {
+                rpn.push_back(' ');
+            }
+        }
+    }
+
+    //后缀表达式求值
+    int rpnLen = rpn.length();
+    for(int i=0; i<rpnLen; i++) {
+        char chr = rpn[i];
+        if(isSym(chr)) {
+            int a, b;
+            int temp;
+            getTwo(a, b, OPND);
+            if(!baseCal(a, b, chr, temp)) return false;
+            else OPND.push(temp);
+        }
+        else if(isNum(chr)) { //以空格分隔，获取操作数
+            
+            OPND.push(chr);
+        }
+    }
+    ans = OPND.top();
 
     return true;
 }
@@ -89,7 +178,10 @@ int main() {
     int i;
     cin>>i;
     while(i--) {
-        cout<<getQues(10, 5, 1)<<endl;
+        int ans;
+        string eq = getQues(10, 5, 1);
+        calEq(eq, ans);
+        cout<<eq<<" = "<<ans<<endl;
     }
 
     return 0;
