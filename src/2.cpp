920078a3cc425926ace794637ae13d299644b488
zyc-816 <860816655@qq.com>
2025-11-21 22:43:34 +0800

    2.0.6 初步实现计算功能（未能除去多余括号）

--- src/2.cpp
+++ src/2.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string>
 #include <stack>
+#include <iomanip>
+#include <sstream>
 using namespace std;
 
 //节点类
@@ -13,12 +15,12 @@ public:
         opt,
         val,
     };
-    int val;
+    double val;
     char opt;
     NodeType type;
     Node* l;
     Node* r;
-    Node(int _val) {
+    Node(double _val) {
         this->type = NodeType::val;
         this->l = nullptr;
         this->r = nullptr;
@@ -40,6 +42,7 @@ public:
     Eq(int deep) {
         this->_deep = deep;
         initTree(deep, this->_root);
+        this->_ans = setAns();
     }
     ~Eq() {
         delTree(this->_root);
@@ -48,12 +51,40 @@ public:
     string getQues() {
         return midOrder(this->_root);
     }
-    //后序遍历得到后缀表达式并计算
-    string getAns();
+
+    double getAns() {
+        return this->_ans;
+    }
+
+    //获取后缀表达式（测试用）
+    string getRpn() {
+        stack<Node*> st1;
+        stack<Node*> st2;
+        Node* cur = this->_root;
+        st1.push(cur);
+        while(!st1.empty()) {
+            cur = st1.top();
+            st1.pop();
+            st2.push(cur);
+            if(cur->l) st1.push(cur->l);
+            if(cur->r) st1.push(cur->r);
+        }
+
+        string rpn = "";
+        while(!st2.empty()) {
+            Node* temp = st2.top();
+            st2.pop();
+            if(temp->type == Node::NodeType::opt) rpn += temp->opt;
+            else rpn.append(to_string((int)temp->val));
+        }
+        return rpn;
+    }
+    
 private:
     int _deep;
     Node* _root;
-    int getNum(int maxNum = 100) {
+    double _ans;
+    int getNum(int maxNum = 10) {
         int ranNum = rand()%(maxNum+1);
         return ranNum;
     }
@@ -64,7 +95,7 @@ private:
     }
     void initTree(int deep, Node*& root) {
         if(deep == 1) {
-            int num = getNum();
+            double num = getNum();
             root = new Node(num);
             return;
         }
@@ -84,29 +115,79 @@ private:
     }
 
     string midOrder(Node* root) {
-        if(root->l == nullptr && root->r == nullptr) return to_string(root->val);
+        if(root->l == nullptr && root->r == nullptr) return doubleToString(root->val);
         if(root->type == Node::NodeType::opt) {
-            return midOrder(root->l) + root->opt + midOrder(root->r);
+            return '(' + midOrder(root->l) + root->opt + midOrder(root->r) + ')';
         }
         return "";
     }
-};
 
-//获取一个随机题目
-//toDo：去除多余括号，算式合法性
-
-bool isSym(char chr) {
-    char sym[] = {'+', '-', '*', '/'};
-    for(int i=0; i<4; i++) {
-        if(chr == sym[i]) return true;
+    //从操作数栈中获取前两个数
+    void getTwo(double& a, double& b, stack<double>& OPND) {
+        b = OPND.top();
+        OPND.pop();
+        a = OPND.top();
+        OPND.pop();
+        return;
     }
-    return false;
-}
 
-bool isNum(char chr) {
-    return chr >= '0' && chr <='9';
-}
+    //处理过程中的基本算式
+    bool Cal(double a, double b, char optr, double& ans) {
+        if(optr == '+') ans = a + b;
+        else if(optr == '-') ans = a - b;
+        else if(optr == '*') ans = a * b;
+        else if(optr == '/') {
+            if(b == 0) {
+                return false;
+            }
+            else ans = a / b;
+        }
 
+        return true;
+    }
+
+    //后序遍历得到后缀表达式并计算
+    double setAns() {
+        stack<Node*> st1;
+        stack<Node*> st2;
+        Node* cur = this->_root;
+        st1.push(cur);
+        while(!st1.empty()) {
+            cur = st1.top();
+            st1.pop();
+            st2.push(cur);
+            if(cur->l) st1.push(cur->l);
+            if(cur->r) st1.push(cur->r);
+        }
+
+        stack<double> OPND;
+        while(!st2.empty()) {
+            Node* temp = st2.top();
+            st2.pop();
+            if(temp->type == Node::NodeType::val) {
+                OPND.push(temp->val);
+            } else if(temp->type == Node::NodeType::opt) {
+                double a, b;
+                double t;
+                getTwo(a, b, OPND);
+                if(!Cal(a, b, temp->opt, t)) {
+                    initTree(this->_deep, this->_root);
+                    this->_ans = this->setAns();
+                    return this->_ans;
+                }
+                OPND.push(t);
+            }
+        }
+        return OPND.top();
+    }
+
+    //将浮点数转为字符串
+    string doubleToString(double val) {
+        stringstream stream; 
+        stream << fixed << setprecision(1) << val;
+        return stream.str();
+    }
+};
 
 //初始化题库
 void initQues() {
@@ -114,111 +195,15 @@ void initQues() {
     return;
 }
 
-//计算问题
-
-//运算符优先级比较
-bool compare(char chr, char top) {
-    if(top == '(') return true;
-    int pri_1 = 0;
-    int pri_2 = 0;
-    if(chr == '*' || chr == '/') pri_1 = 1;
-    if(top == '*' || top == '/') pri_2 = 1;
-    return pri_1 > pri_2;
-}
-
-//从操作数栈中获取前两个数
-void getTwo(int& a, int& b, stack<int>& OPND) {
-    a = OPND.top();
-    OPND.pop();
-    b = OPND.top();
-    OPND.pop();
-    return;
-}
-
-//处理过程中的基本算式
-bool baseCal(int a, int b, char optr, int& ans) {
-    if(optr == '+') ans = a + b;
-    else if(optr == '-') ans = a - b;
-    else if(optr == '*') ans = a * b;
-    else if(optr == '/') {
-        if(b == 0) {
-            return false;
-        }
-        else ans = a / b;
-    }
-
-    return true;
-}
-
-//计算问题
-bool calEq(string ques, int& ans) {
-    stack<int> OPND; //操作数栈
-    stack<char> OPTR; //操作符栈
-
-    //转化为后缀表达式
-    string rpn = "";
-    int len = ques.length();
-    for(int i=0; i<len; i++) {
-        char chr = ques[i];
-        if(isSym(chr) || chr == '(' || chr == ')') {
-            if(chr == ')') {
-                while(OPTR.top() != '(') {
-                    rpn.push_back(OPTR.top());
-                    OPTR.pop();
-                }
-                OPTR.pop();
-            }
-            else if(OPTR.empty() || compare(chr, OPTR.top())) {
-                OPTR.push(chr);
-            }
-            else {
-                while(!compare(chr, OPTR.top())) {
-                    rpn.push_back(OPTR.top());
-                    OPTR.pop();
-                }
-                OPTR.push(chr);
-            }
-        }
-        else if(isNum(chr)) {
-            rpn.push_back(chr);
-
-            //以空格隔开数字，便于处理
-            if(i+1 < len 
-                && (isSym(ques[i+1]) || chr == '(' || chr == ')')) {
-                rpn.push_back(' ');
-            }
-        }
-    }
-
-    //后缀表达式求值
-    int rpnLen = rpn.length();
-    for(int i=0; i<rpnLen; i++) {
-        char chr = rpn[i];
-        if(isSym(chr)) {
-            int a, b;
-            int temp;
-            getTwo(a, b, OPND);
-            if(!baseCal(a, b, chr, temp)) return false;
-            else OPND.push(temp);
-        }
-        else if(isNum(chr)) { //以空格分隔，获取操作数
-            
-            OPND.push(chr);
-        }
-    }
-    ans = OPND.top();
-
-    return true;
-}
-
 int main() {
     srand((unsigned)time(NULL));
     initQues();
+    cout<<setiosflags(ios::fixed)<<setprecision(1); //设置精度
     int i;
     cin>>i;
     while(i--) {
-        Eq eq(4);
-        cout<<eq.getQues()<<endl;
+        Eq eq(3);
+        cout<<eq.getQues()<<" = "<<eq.getAns()<<endl;
     }
 
     return 0;
